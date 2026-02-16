98b63ed091a21fbe977e953b3a66cc7baa0b07e1
Arthuro08 <arthur.gomes0803@gmail.com>
2026-02-16 18:14:23 -0300

    added a new solution in C (calculadora)

---
+++ solucoes_em_c/calculadora.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+double calc(double x1, char operador, double x2){
+    switch(operador){
+        case '+': return x1 + x2;
+        case '-': return x1 - x2;
+        case '*': return x1 * x2;
+        case '/': return x1 / x2;
+        case 'e': return pow(x1, x2);
+        case 'r': return sqrt(x1);
+        default: return 0;
+    }
+}
+
+int main(){
+    double x1, x2, n, result;
+    char operador;
+    printf("Digite a operação desejada:\n1. '+' para adição;\n2. '-' para subtração;\n3. '*' para multiplicação;\n4. '/' para divisão;\n5. 'e' para exponenciação;\n6. 'r' para radiciação.\n");
+    printf("________________________________________________________________\nDigite a entrada: ");
+    scanf("%lf %c %lf", &x1, &operador, &x2);
+    if(operador!='+' && operador!='-' && operador!='*' && operador!='/' && operador!='e' && operador!='r'){
+        printf("Operador inválido!\n");
+    } else{
+        if(operador=='r'){
+        result = calc(x1,operador,0);
+        } else{
+        result = calc(x1,operador,x2);
+        }
+        printf("Resultado: %.2lf", result);
+    }
+    
+    return 0;
+}
