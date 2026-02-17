fb465ca11269c0ced29e2645de3e1490aec7d60f
Arthuro08 <arthur.gomes0803@gmail.com>
2026-02-17 13:29:02 -0300

    Implementa mais tratamentos de erros e loop condicional.

--- solucoes_em_c/calculadora.c
+++ solucoes_em_c/calculadora.c
@@ -15,20 +15,27 @@ double calc(double x1, char operador, double x2){
 
 int main(){
     double x1, x2, result;
-    char operador;
-    printf("Digite a operação desejada:\n1. '+' para adição;\n2. '-' para subtração;\n3. '*' para multiplicação;\n4. '/' para divisão;\n5. 'e' para exponenciação;\n6. 'r' para radiciação.\n");
-    printf("________________________________________________________________\nDigite a entrada: ");
-    scanf("%lf %c %lf", &x1, &operador, &x2);
-    if(operador!='+' && operador!='-' && operador!='*' && operador!='/' && operador!='e' && operador!='r'){
-        printf("Operador inválido!\n");
-    } else{
-        if(operador=='r'){
-        result = calc(x1,operador,0);
-        } else{
-        result = calc(x1,operador,x2);
+    char operador, decisao;
+    do{
+        printf("Digite a operação desejada:\n1. '+' para adição;\n2. '-' para subtração;\n3. '*' para multiplicação;\n4. '/' para divisão;\n5. 'e' para exponenciação;\n6. 'r' para radiciação.\n");
+        printf("________________________________________________________________\nDigite a entrada: ");
+        scanf("%lf %c", &x1, &operador);
+        if(operador != 'r'){
+            scanf("%lf", &x2);
         }
-        printf("Resultado: %.2lf", result);
-    }
-    
-    return 0;
+        if(operador!='+' && operador!='-' && operador!='*' && operador!='/' && operador!='e' && operador!='r'){
+            printf("Operador inválido!\n");
+        } else if(operador == '/' && x2 == 0){
+            printf("ERRO: Divisão por zero não existe!\n");
+        } else if(operador == 'r' && x1 < 0){
+            printf("ERRO: Não existe raiz quadrada de número negativo!\n");
+        } else{   
+            result = calc(x1,operador,x2);
+            printf("Resultado: %.2lf", result);
+        }
+        printf("\nDeseja continuar a fazer operações? (S ou N): ");
+        scanf(" %c", &decisao);
+        } while(decisao == 'S' || decisao == 's');
+        printf("Operações finalizadas com êxito.");
+        return 0;
 }
