4d2d637cf1873e90e6ae467857200a2de949720d
Arthuro08 <arthur.gomes0803@gmail.com>
2026-05-19 16:54:03 -0300

    creating API

--- sistemas_com_sql/database ficha medica/fichaAPI.py
+++ sistemas_com_sql/database ficha medica/fichaAPI.py
@@ -1,4 +1,8 @@
 import pyodbc
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+app.secret_key = 'ArthurAlmeidaFichaAPI'
 
 config = pyodbc.connect(
     'Driver={SQL Server};'
@@ -6,3 +10,10 @@ config = pyodbc.connect(
     'Database=Users_Sistema;'
     'Trusted_Connection=yes;'
 )
+
+cursor = config.cursor()
+
+@app.route('/receber', methods=['GET'])
+def receber():
+    pass
+
