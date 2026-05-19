1720bc8dbac0003d8fd4705c67e5050244516277
Arthuro08 <arthur.gomes0803@gmail.com>
2026-05-19 17:15:35 -0300

    perf

--- sistemas_com_sql/database ficha medica/fichaAPI.py
+++ sistemas_com_sql/database ficha medica/fichaAPI.py
@@ -15,8 +15,22 @@ cursor = config.cursor()
 
 @app.route('/receber', methods=['POST'])
 def receber():
-    nome = request.form['nome_paciente']
+    nome_paciente = request.form['nome_paciente']
     rg = request.form['RG']
     sexo = request.form['sexo']
     convenio = request.form['convenio']
+    data_nasc = request.form['data_nasc']
+    estado_civil = request.form['est_civil']
+
+    endereco = request.form['endereco']
+    telefone = request.form['telefone']
+
+    data_consulta = request.form['data_consulta']
+    medico = request.form['medico']
+    diagnostico = request.form['diagnostico']
+
+    exame = request.form['exame']
+    data_exame = request.form['data_exame']
+
+
 
