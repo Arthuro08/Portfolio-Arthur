22b306e3f751e93496bd3a592737da294ceab13e
Arthuro08 <arthur.gomes0803@gmail.com>
2026-06-03 12:24:08 -0300

    move files to respective folders v3

--- sistemas_com_sql/database concessionaria/arquivos sql/insert_Concessionaria.sql
+++ sistemas_com_sql/database concessionaria/arquivos sql/insert_Concessionaria.sql
@@ -143,10 +143,3 @@ VALUES
 ('2025-05-28', 272000.00, 29),
 ('2025-06-03', 335000.00, 22),
 ('2025-06-10', 215000.00, 25);
-
-ALTER TABLE Marca ADD CONSTRAINT UQ_Marca_Nome UNIQUE (Nome) /* restrição (ou constraint) para a coluna Nome da tabela Marca (possuir apenas dados UNIQUE) */
-ALTER TABLE Modelo ADD CONSTRAINT UQ_Modelo_Nome UNIQUE (Nome)
-ALTER TABLE Venda ADD CONSTRAINT UQ_Venda_FK_ID_Carro UNIQUE (FK_ID_Carro) /* restrição na FK do Carro da tabela Venda para evitar que um carro seja vendido duas vezes */
-
-SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE TABLE_NAME = 'Marca' /* seleciona todas as constraints criadas na tabela 'Marca' (Ex: a PK e o UNIQUE) */
-SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE TABLE_NAME = 'Modelo' /* seleciona todas as constraints criadas na tabela 'Modelo' (Ex: a PK, a FK e o UNIQUE) */
